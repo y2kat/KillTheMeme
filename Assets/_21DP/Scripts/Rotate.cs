0034b365c6df58d6d72fba38440d674192b320fb
y2kat.ia <lentacomputadora@gmail.com>
2022-04-27 15:05:57 -0500

    4chan health rotate

---
+++ Assets/_21DP/Scripts/Rotate.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    private void Update()
+    {
+        transform.Rotate(new Vector3(0f, 100f, 0f) * Time.deltaTime);
+    }
+}
