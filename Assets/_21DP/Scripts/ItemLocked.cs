ac139eb6cbc18fcff0fcc3a0cae67ab983cf02a4
y2kat.ia <lentacomputadora@gmail.com>
2022-05-04 08:08:41 -0500

    commit

--- Assets/_21DP/Scripts/ItemLocked.cs
+++ Assets/_21DP/Scripts/ItemLocked.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ItemLocked : Interactable
 {
     [SerializeField]
     private int necesaryItems = 3;
+
+    public int index;
 
     public override void OnInteraction()
     {
@@ -21,5 +24,12 @@ public class ItemLocked : Interactable
     {
         Debug.Log("Open");
         GetComponent<Animator>().enabled = true;
+        StartCoroutine(ChangeScene());
+    }
+
+    IEnumerator ChangeScene()
+    {
+        yield return new WaitForSeconds(2f);
+        SceneManager.LoadScene(index);
     }
 }
